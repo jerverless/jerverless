ee38a4241bd2c8359fa5c6310c6e694ba1eedee7
Shalitha <shalithasuranga@gmail.com>
2018-11-10 12:43:52 +0530

    Add project structure

---
+++ src/main/java/org/jerverless/boot/BootServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jerverless.boot;
+
+/**
+ *
+ * Boots the jerverless server with command line
+ */
+public class BootServer {
+    /**
+     * main method 
+     * 
+     * @param args 
+     * @return 
+     */
+    public static void main(String[] args) {
+        System.out.println("Bootstrap logic..");
+    }
+}
