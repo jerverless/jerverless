ee049f86d47bee05a587d02e469744851d1895ca
Shalitha Suranga <shalithasuranga@gmail.com>
2018-11-19 10:44:21 +0530

    Update HelloWorld.java

--- examples/java/HelloWorld.java
+++ examples/java/HelloWorld.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        String name = sc.nextLine(); //We don't handle exception or line check ;)
         System.out.println("Hello " + name + "!");
     }
 }
