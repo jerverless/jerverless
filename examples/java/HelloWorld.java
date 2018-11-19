82af44e162c2557308ed313fdfbd7bb491c59993
Shalitha <shalithasuranga@gmail.com>
2018-11-19 08:49:49 +0530

    Add java example

---
+++ examples/java/HelloWorld.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+}
