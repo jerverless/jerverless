1c3cee1c713ac7dd2673731af9b8e2cc9e1143e3
Kasun Vithanage <alankasun@gmail.com>
2019-01-19 14:19:53 +0530

    updated docs with examples
    Signed-off-by: Kasun Vithanage <alankasun@gmail.com>

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import sys
 
 print("input = " + raw_input())
 
-if len(sys.argv) == 2 and sys.argv[1] == '--foo':
-    print "boom!"
+if len(sys.argv) == 2 and sys.argv[1] == '--bar':
+    print "bar!"
