2fd6aa214b5337bb5f3d65670a34b31e60dfe33e
Kasun Vithanage <alankasun@gmail.com>
2019-01-20 09:27:18 +0530

    Revert "Feature/docker builds"

--- .travis/docker-push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-export REPO=jerverless/jerverless
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_TAG ; fi`
-docker build -f Dockerfile -t $REPO:$TAG .
-docker push $REPO
