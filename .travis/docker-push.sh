5005c041b096ed47fda9e1e8aab57d4cfb5b6e0e
Kasun Vithanage <alankasun@gmail.com>
2019-01-20 10:06:04 +0530

    script updated

--- .travis/docker-push.sh
+++ .travis/docker-push.sh
@@ -3,5 +3,8 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 export REPO=jerverless/jerverless
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_TAG ; fi`
+echo Tagging $REPO with $TAG
 docker build -f Dockerfile -t $REPO:$TAG .
+echo Pushing image to Docker Hub
 docker push $REPO
+echo Done
